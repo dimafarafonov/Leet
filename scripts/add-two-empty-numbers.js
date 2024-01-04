cd8762abf4cb1d1f566f8b34f33bd2edaa26dc95
DiiF <dimafarafonovzt@gmail.com>
2024-01-04 18:22:48 +0200

    finishing!!

--- scripts/add-two-empty-numbers.js
+++ scripts/add-two-empty-numbers.js
@@ -12,84 +12,56 @@
  */
 let addTwoNumbers = function (l1, l2) {
   const recursion = (list, acc) => {
-    if (!list?.val || !list["next"]) {
+    if (list["val"] === undefined || !list["next"]) {
+      // Wrong Answer 1565 / 1568 testcases passed
       // return if no data due to conditions
-      return `${acc + (list?.val ? list.val : 0)}`;
+      return `${acc + (list.val ? list.val : 0)}`;
     }
 
-    let val = list.val;
     return recursion(list["next"], `${acc + list.val}`);
   };
-
-  const firstList = recursion(l1, 0).split("").reverse().join("");
-
-  console.log(firstList);
-
-  const secondList = recursion(l2, 0).split("").reverse().join("");
-
-  console.log(secondList);
-
-  const outputStage1 = String(Number(firstList) + Number(secondList))
-    .split("")
-    .reverse();
-
-  console.log(outputStage1);
-
-  const reverseRecursion = (array, acc) => {
+  const reverseRecursion = (array, current, sum) => {
     if (!array?.length) {
       // return if no data due to conditions
-      return acc;
+      return current;
     }
 
-    console.log("acc", acc);
-
-    // for (let keyNext in acc) {
-    //   if (keyNext === "next") {
-    //     if (!acc[keyNext]) {
-    //       acc[keyNext] = { val: array[0], next: null };
-    //     }
-    //   }
-    //   console.log("nextValue", keyNext);
-    // }
-
-    // if (!Object.keys(acc).length) { // for first init
-    //   acc = { val: array[0], next: null };
-    // }
+    current.next = { val: array[0], next: null };
 
     // could be the solution, but not completed
     array.shift();
 
-    return reverseRecursion(array, acc);
+    return reverseRecursion(array, current.next);
   };
 
-  const output = reverseRecursion(outputStage1, {});
+  const firstList = recursion(l1, 0).split("").reverse().join("");
 
-  console.log("123", output);
-  //   let l1Sum = l1
-  //     .reverse()
-  //     .reduce((acc, currentValue) => String(acc) + String(currentValue));
-  //   let l2Sum = l2
-  //     .reverse()
-  //     .reduce((acc, currentValue) => String(acc) + String(currentValue));
-  //   let finalArray = String(Number(l1Sum) + Number(l2Sum))
-  //     .split("")
-  //     .map((item) => {
-  //       return +item;
-  //     });
-  //   console.log(finalArray);
-  // wrong implementation, should be
+  const secondList = recursion(l2, 0).split("").reverse().join("");
+
+  const outputStage1 = String(BigInt(firstList) + BigInt(secondList)) // BigInt was the saver to represent 1e+43 number without exp annotation 
+    .split("")
+    .reverse();
+
+
+  const head = { val: outputStage1[0], next: null }; // key point, solved with chatgpt help
+  outputStage1.shift();
+
+  reverseRecursion(outputStage1, head, {});
+
+  console.log("head", head); // Wrong Answer 284 / 1568 testcases passed
+  return head;
 };
 
-const list1 = {
-  val: 2,
-  next: {
-    val: 4,
-    next: {
-      val: 3,
-      next: null,
-    },
-  },
-};
+// const list1 = {
+//   val: 1,
+//   next: {
+//     val: 0,
+//     next: {
+//       val: 9,
+//       next: null,
+//     },
+//   },
+// };
 
 const list2 = {
   val: 5,
@@ -102,4 +74,152 @@ const list2 = {
   },
 };
 
+const list1 = {
+  val: 1,
+  next: {
+    val: 0,
+    next: {
+      val: 0,
+      next: {
+        val: 0,
+        next: {
+          val: 0,
+          next: {
+            val: 0,
+            next: {
+              val: 0,
+              next: {
+                val: 0,
+                next: {
+                  val: 0,
+                  next: {
+                    val: 0,
+                    next: {
+                      val: 0,
+                      next: {
+                        val: 0,
+                        next: {
+                          val: 0,
+                          next: {
+                            val: 0,
+                            next: {
+                              val: 0,
+                              next: {
+                                val: 0,
+                                next: {
+                                  val: 0,
+                                  next: {
+                                    val: 0,
+                                    next: {
+                                      val: 0,
+                                      next: {
+                                        val: 0,
+                                        next: {
+                                          val: 0,
+                                          next: {
+                                            val: 0,
+                                            next: {
+                                              val: 0,
+                                              next: {
+                                                val: 0,
+                                                next: {
+                                                  val: 0,
+                                                  next: {
+                                                    val: 0,
+                                                    next: {
+                                                      val: 0,
+                                                      next: {
+                                                        val: 0,
+                                                        next: {
+                                                          val: 0,
+                                                          next: {
+                                                            val: 0,
+                                                            next: {
+                                                              val: 0,
+                                                              next: {
+                                                                val: 0,
+                                                                next: {
+                                                                  val: 0,
+                                                                  next: {
+                                                                    val: 0,
+                                                                    next: {
+                                                                      val: 0,
+                                                                      next: {
+                                                                        val: 0,
+                                                                        next: {
+                                                                          val: 0,
+                                                                          next: {
+                                                                            val: 0,
+                                                                            next: {
+                                                                              val: 0,
+                                                                              next: {
+                                                                                val: 0,
+                                                                                next: {
+                                                                                  val: 0,
+                                                                                  next: {
+                                                                                    val: 0,
+                                                                                    next: {
+                                                                                      val: 0,
+                                                                                      next: {
+                                                                                        val: 1,
+                                                                                        next: null,
+                                                                                      },
+                                                                                    },
+                                                                                  },
+                                                                                },
+                                                                              },
+                                                                            },
+                                                                          },
+                                                                        },
+                                                                      },
+                                                                    },
+                                                                  },
+                                                                },
+                                                              },
+                                                            },
+                                                          },
+                                                        },
+                                                      },
+                                                    },
+                                                  },
+                                                },
+                                              },
+                                            },
+                                          },
+                                        },
+                                      },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
 export { addTwoNumbers, list1, list2 };
+
+//   let l1Sum = l1
+//     .reverse()
+//     .reduce((acc, currentValue) => String(acc) + String(currentValue));
+//   let l2Sum = l2
+//     .reverse()
+//     .reduce((acc, currentValue) => String(acc) + String(currentValue));
+//   let finalArray = String(Number(l1Sum) + Number(l2Sum))
+//     .split("")
+//     .map((item) => {
+//       return +item;
+//     });
+//   console.log(finalArray);
+// wrong implementation, should be
